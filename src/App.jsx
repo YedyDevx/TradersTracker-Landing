8ca0a5017d38544726a1e2d1051b096d80cc4fba
YedixDev <yedixonjrf@gmail.com>
2025-01-16 14:31:07 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold p-8 text-red-600">¡Tailwind está funcionando!</h1>
+    </div>
+  );
+}
+export default App;

c18ef333bd63c2b5cc274013bd203858fe8e7bf8
YedixDev <yedixonjrf@gmail.com>
2025-01-18 11:16:59 -0500

    Integracion de la tercera seccion

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import {  Element } from 'react-scroll';
 import Section1 from "./Sections/Section-1";
 import Section2 from "./Sections/Section-2";
+import Section3 from "./Sections/Section-3";
 
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
                 <Element name="newsletter">
                     <Section2 />
                 </Element>
+                <Section3/>
                 <Element name="plans" className="h-screen bg-yellow-500">
                     
                 </Element>

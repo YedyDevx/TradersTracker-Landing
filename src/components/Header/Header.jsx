800fa30abe58c2a806cc51c9fbe15766f52bcf42
YedixDev <yedixonjrf@gmail.com>
2025-01-20 15:41:42 -0500

    Ajuste en el header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-// import { Link } from 'react-scroll';
+import { Link } from 'react-scroll';
 import ThemeChange from '../ThemeChange/ThemeChange';
 import logo from '../../assets/logo-sin-fondo.png';
 
@@ -17,7 +17,7 @@ const Header = () => {
       </div>
       <div className="text-md font-semibold flex flex-row gap-16 text-gray-900 dark:text-gray-300">
         {navItems.map((item) => (
-          <a
+          <Link
             key={item.to}
             to={item.to}
             smooth={true}
@@ -25,7 +25,7 @@ const Header = () => {
             className="cursor-pointer hover:bg-[#fdbf22] hover:shadow-[inset_0_3px_8px_rgba(0,0,0,0.5)] px-6 py-1 rounded-lg dark:hover:text-black transition-all duration-500 ease-in-out"
             >
             {item.name}
-          </a>
+          </Link>
         ))}
       </div>
       <div className="flex flex-row items-center gap-4">
